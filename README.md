f7df0a6123674ea85053757b4ca95a06b6f8c8f9
claviering <claviering@gmail.com>
2018-07-18 19:06:33 +0800

    completed

---
+++ README.md
@@ -0,0 +1,5 @@
+# 微信小程序计算器实现
+
+## 效果图
+![1](./pages/static/img/calc.png)
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,45 @@
 //index.js
+var math = require('./math.js')
 //获取应用实例
 const app = getApp()
 
+
 Page({
   data: {
-    items:["AC","Del","/","X","7","8","9","-","4","5","6","+","1","2","3","=","%","0","."]
+    items:["AC","Del","/","*","7","8","9","-","4","5","6","+","1","2","3","=","%","0","."],
+    str:"",
+    res:"0",
+  },
+  tap: function(e){
+    var ch = e.target.id;
+    if(ch === "="){
+      this.equal();
+      return;
+    }
+    else if (ch === "AC"){
+      this.change(0);
+      this.data.str = "";
+      this.change("0");
+      return;
+    }
+    else if (ch === "Del"){
+      this.data.str = this.data.str.slice(0, -1);
+      if (this.data.str === ""){
+        this.change("0");
+      }
+      else{
+        this.change(this.data.str);
+      }
+      return;
+    }
+    this.data.str += ch;
+    this.change(this.data.str);
+  },
+  equal: function(e){
+    var result = math.eval(this.data.str);
+    this.change(result);
+  },
+  change:function(str){
+    this.setData({res:str})
   }
 })
