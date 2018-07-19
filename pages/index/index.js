e10bb8244414cc218f0faa4627a1a159cf2fec9d
claviering <claviering@gmail.com>
2018-07-19 21:59:52 +0800

    完成科学计算js代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,43 +13,91 @@ Page({
     res:"0",
     tran:["big","small"],
     tran_flag:0,
-    index:0
+    index:0,
+    stack:[],
   },
-  tap: function(e){
+  calc: function(e){
     var ch = e.target.id;
-    if(ch === "="){
-      this.equal();
-      return;
+    switch(ch){
+      case "=":
+        this.equal();
+        return;
+      case "AC":
+        this.data.str = "";
+        this.change_res("0");        
+        return;
+      case "Del":
+        this.data.str = this.data.str.slice(0, -1);
+        if (this.data.str === ""){
+          this.change_res("0");
+        }
+        else{
+          this.change_res(this.data.str);
+        }
+        return;
+      case "sin":
+        this.data.str += ch+"(";
+        break;
+      case "cos":
+        this.data.str += ch+"(";
+        break;
+      case "tan":
+        this.data.str += ch+"(";
+        break;
+      case "lg":
+        this.data.stack.push("lg");
+        this.data.str += ch+"(";
+        break;
+      case "ln":
+        this.data.stack.push("ln");
+        this.data.str += ch+"(";
+        break;
+      case ")":
+        if (this.data.stack.length > 0){
+          var tmp = this.data.stack.pop();
+          if (tmp === "lg"){
+            this.data.str += ",10";
+          }
+          else if (tmp === "ln"){
+            this.data.str += ",e";
+          }
+        }
+        this.data.str += ")";
+        break;
+      case "sqr":
+        this.data.str += "sqrt(";
+        break;
+      case "1/X":
+        this.data.str = "1/(" + this.data.str+")";
+        break;
+      case "2nd":
+        return;
+        break;
+      case "deg":
+        return;
+        break;
+      default:
+        this.data.str += ch;
     }
-    else if (ch === "AC"){
-      this.change(0);
-      this.data.str = "";
-      this.change("0");
-      return;
-    }
-    else if (ch === "Del"){
-      this.data.str = this.data.str.slice(0, -1);
-      if (this.data.str === ""){
-        this.change("0");
-      }
-      else{
-        this.change(this.data.str);
-      }
-      return;
-    }
-    this.data.str += ch;
-    this.change(this.data.str);
+    this.change_res(this.data.str);    
+    
   },
-  equal: function(e){
+  equal: function(){
+    this.data.str = this.data.str.replace(/lg/gi,"log");
+    this.data.str = this.data.str.replace(/ln/gi,"log");
     var result = math.eval(this.data.str);
-    this.change(result);
+    this.change_res(result);
   },
-  change:function(str){
+  change_res:function(str){
     this.setData({res:str})
   },
-  tran_fun:function(){
+  change_mode:function(){
     this.setData({tran_flag:!this.data.tran_flag});
     var tran_index = this.data.tran_flag ? 1 : 0;
     this.setData({index:tran_index});
+  },
+  run:function(){
+    var str = "log(100,e)";
+    console.log(math.eval(str));
   }
 })
